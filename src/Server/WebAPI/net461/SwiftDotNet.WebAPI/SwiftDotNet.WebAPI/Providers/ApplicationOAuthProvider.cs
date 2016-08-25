7fe071b8218ca405837b2a92ea521e84be99519c
Mark Hamilton <mark@mark-hamilton.com>
2016-08-24 19:56:47 -0700

    Cleanup

--- src/Server/WebAPI/net461/SwiftDotNet.WebAPI/SwiftDotNet.WebAPI/Providers/ApplicationOAuthProvider.cs
+++ src/Server/WebAPI/net461/SwiftDotNet.WebAPI/SwiftDotNet.WebAPI/Providers/ApplicationOAuthProvider.cs
@@ -10,6 +10,7 @@ using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
 using SwiftDotNet.WebAPI.Models;
+using SwiftDotNet.WebAPI.Helpers;
 
 namespace SwiftDotNet.WebAPI.Providers
 {
