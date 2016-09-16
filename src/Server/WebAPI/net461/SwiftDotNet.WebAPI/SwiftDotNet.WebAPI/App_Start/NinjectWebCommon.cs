e9e36af322994a6df9dbb814df8d94a96de0339b
Mark Hamilton <mark@mark-hamilton.com>
2016-09-16 00:10:07 -0700

    Update Ninject

---
+++ src/Server/WebAPI/net461/SwiftDotNet.WebAPI/SwiftDotNet.WebAPI/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftDotNet.WebAPI.App_Start
+{
+    class NinjectWebCommon
+    {
+    }
+}
--- src/Server/WebAPI/net461/SwiftDotNet.WebAPI/SwiftDotNet.WebAPI/Controllers/api/AnalyticsController.cs
+++ src/Server/WebAPI/net461/SwiftDotNet.WebAPI/SwiftDotNet.WebAPI/Controllers/api/AnalyticsController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNet.SignalR;
 using Newtonsoft.Json;
+using SwiftDotNet.WebAPI.Entities.Analytics;
 using SwiftDotNet.WebAPI.Hubs;
 using SwiftDotNet.WebAPI.Models;
+using SwiftDotNet.WebAPI.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,13 +21,13 @@ namespace SwiftDotNet.WebAPI.Controllers.api
 
         #region ctors
 
-        //private readonly IAnalyticRepository _repo;
+        private readonly IAnalyticRepository _repo;
         private readonly IHubContext _context;
         private static HttpClient _client = new HttpClient();
 
-        public AnalyticsController() //(IAnalyticRepository repo)
+        public AnalyticsController(IAnalyticRepository repo)
         {
-            //this._repo = repo;
+            this._repo = repo;
             this._context = GlobalHost.ConnectionManager.GetHubContext<AnalyticsHub>();
         }
 
@@ -37,43 +39,43 @@ namespace SwiftDotNet.WebAPI.Controllers.api
         /// Gets a list of all Analytics in the database.
         /// </summary>
         /// <returns></returns>
-        //public IEnumerable<Analytic> GetAllInCollection()
-        //{
-        //    var result = _repo.Get();
+        public IEnumerable<Analytic> GetAllInCollection()
+        {
+            var result = _repo.Get();
 
-        //    return result;
-        //}
+            return result;
+        }
 
-        ///// <summary>
-        ///// Gets an Analytic in the Database by Id.
-        ///// </summary>
-        ///// <param name="id">The Analytic's GUID.</param>
-        ///// <returns></returns>
-        //public Task<Analytic> GetById(string id)
-        //{
-        //    return _repo.GetById(id);
-        //}
+        /// <summary>
+        /// Gets an Analytic in the Database by Id.
+        /// </summary>
+        /// <param name="id">The Analytic's GUID.</param>
+        /// <returns></returns>
+        public Task<Analytic> GetById(string id)
+        {
+            return _repo.GetById(id);
+        }
 
-        ///// <summary>
-        ///// Creates a new Analytic in the Database and passes it to the AnalyticsHub.
-        ///// </summary>
-        ///// <param name="entity"></param>
-        ///// <returns>An HTTP Status code - 200 (OK) or 400 (Bad Request)</returns>
-        //public async Task<IHttpActionResult> Post([FromBody] Analytic entity)
-        //{
-        //    var result = await _repo.CreateDocumentAsync(entity);
-        //    var id = result.Resource.Id;
-        //    var model = _repo.GetById(id);
+        /// <summary>
+        /// Creates a new Analytic in the Database and passes it to the AnalyticsHub.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns>An HTTP Status code - 200 (OK) or 400 (Bad Request)</returns>
+        public async Task<IHttpActionResult> Post([FromBody] Analytic entity)
+        {
+            var result = await _repo.CreateDocumentAsync(entity);
+            var id = result.Resource.Id;
+            var model = _repo.GetById(id);
 
-        //    var hubEventParameters = new
-        //    {
-        //        Analytic = entity
-        //    };
+            var hubEventParameters = new
+            {
+                Analytic = entity
+            };
 
-        //    _context.Clients.All.locationReceived(hubEventParameters);
+            _context.Clients.All.locationReceived(hubEventParameters);
 
-        //    return Ok(model);
-        //}
+            return Ok(model);
+        }
 
         /// <summary>
         /// Updates an existing Analytic in the Database and passes it to the AnalyticsHub.
@@ -81,33 +83,33 @@ namespace SwiftDotNet.WebAPI.Controllers.api
         /// <param name="id">The Analytic's GUID.</param>
         /// <param name="entity"></param>
         /// <returns>An HTTP Status code - 200 (OK) or 400 (Bad Request)</returns>
-        //[System.Web.Http.Authorize]
-        //public async Task<IHttpActionResult> Put(string id, [FromBody]Analytic entity)
-        //{
-        //    await _repo.UpdateDocumentAsync(entity);
-        //    var model = _repo.GetById(id);
+        [System.Web.Http.Authorize]
+        public async Task<IHttpActionResult> Put(string id, [FromBody]Analytic entity)
+        {
+            await _repo.UpdateDocumentAsync(entity);
+            var model = _repo.GetById(id);
 
-        //    // Send Analytic Put to All Clients
-        //    _context.Clients.All.putNewAnalytic(entity);
+            // Send Analytic Put to All Clients
+            _context.Clients.All.putNewAnalytic(entity);
 
-        //    return Ok(model);
-        //}
+            return Ok(model);
+        }
 
         /// <summary>
         /// Deletes an existing Analytic in the database.
         /// </summary>
         /// <param name="id"></param>
         /// <returns>An HTTP Status code - 200 (OK) or 400 (Bad Request)</returns>
-        //[System.Web.Http.Authorize]
-        //public async Task<IHttpActionResult> Delete(string id)
-        //{
-        //    await _repo.DeleteDocumentAsync(id);
+        [System.Web.Http.Authorize]
+        public async Task<IHttpActionResult> Delete(string id)
+        {
+            await _repo.DeleteDocumentAsync(id);
 
-        //    // Remove Analytic from All Clients
-        //    _context.Clients.All.deleteAnalytic(id);
+            // Remove Analytic from All Clients
+            _context.Clients.All.deleteAnalytic(id);
 
-        //    return Ok();
-        //}
+            return Ok();
+        }
 
         /// <summary>
         /// Returns the IP Address and Browser Info of the requester.
