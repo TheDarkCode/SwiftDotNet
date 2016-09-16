1444fd7cb8367ce86797431d290d397c0751e71f
Mark Hamilton <mark@mark-hamilton.com>
2016-09-15 23:57:09 -0700

    Add Analytic Repository

---
+++ src/Server/WebAPI/net461/SwiftDotNet.WebAPI/SwiftDotNet.WebAPI/Repositories/IAnalyticRepository.cs
@@ -0,0 +1,16 @@
+﻿using SwiftDotNet.WebAPI.Entities.Analytics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftDotNet.WebAPI.Repositories
+{
+    /// <summary>
+    /// Add custom members here.
+    /// </summary>
+    public interface IAnalyticRepository : IRepository<Analytic>
+    {
+    }
+}
