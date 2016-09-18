92ff40930b7d4dd7be93f502a17cbfa1150bf42c
Mark Hamilton <mark@mark-hamilton.com>
2016-09-17 23:59:40 -0700

    Update NinjectWebCommon.cs

--- src/Server/WebAPI/net461/SwiftDotNet.WebAPI/SwiftDotNet.WebAPI/App_Start/NinjectWebCommon.cs
+++ src/Server/WebAPI/net461/SwiftDotNet.WebAPI/SwiftDotNet.WebAPI/App_Start/NinjectWebCommon.cs
@@ -1,12 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(DryverlessAds.AffiliateApi.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(DryverlessAds.AffiliateApi.App_Start.NinjectWebCommon), "Stop")]
 
-namespace SwiftDotNet.WebAPI.App_Start
+namespace DryverlessAds.AffiliateApi.App_Start
 {
-    class NinjectWebCommon
+    using System;
+    using System.Web;
+
+    using SwiftDotNet.WebAPI.Repositories;
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+    using Ninject;
+    using Ninject.Web.Common;
+    using SwiftDotNet.WebAPI.Controllers.api;
+    using SwiftDotNet.WebAPI.Repositories;
+
+    public static class NinjectWebCommon
     {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+            kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+            RegisterServices(kernel);
+
+            System.Web.Http.GlobalConfiguration.Configuration.DependencyResolver = new Ninject.WebApi.DependencyResolver.NinjectDependencyResolver(kernel);
+
+            return kernel;
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+            //kernel.Bind<DocumentDbClient>().ToSelf().InRequestScope();
+            kernel.Bind<IAnalyticRepository>().To<AnalyticRepository>().InRequestScope();
+        }
     }
 }
